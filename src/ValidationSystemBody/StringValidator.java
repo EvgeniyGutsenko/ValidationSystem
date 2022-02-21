b7394cb272b0216ffd4d1ad81a650a6518b47475
EvgeniyGutsenko <97789152+EvgeniyGutsenko@users.noreply.github.com>
2022-02-21 16:42:10 +0300

    Initiate new branch

--- src/ValidationSystemBody/StringValidator.java
+++ src/ValidationSystemBody/StringValidator.java
@@ -7,6 +7,6 @@ public class StringValidator implements Validator<String> {
     @Override
     public void validate(String input) throws ValidationFailedException {
         if (!input.matches("^[A-ZА-ЯЁ].+"))
-            throw new ValidationFailedException("The string must start with a capital letter, try again");
+            throw new ValidationFailedException("The string must start with a capital letter, try again!");
     }
 }
